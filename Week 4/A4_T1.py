a9d65289e23c608fa5fdcdb7b67141cdc1028a35
Aliyyu Zen-Abdeen <babatundealiyyu411@gmail.com>
2025-12-17 18:40:06 +0200

    Update A4_T1.py

--- Week 4/A4_T1.py
+++ Week 4/A4_T1.py
@@ -2,12 +2,9 @@ print("Program starting")
 First =int(input("Insert starting value: "))
 Second =int(input("Insert stopping value: "))
 print("Starting for loop:")
-for x in range(1, 11):
+
+for x in range(First, Second):
     print(x)
 
-for x in range(2, 8,):
-    print(x)
-    if x== 8:
-        break
-    else:
-        print(x)
+print("Program ending.")
+
