b703a8c2290b78f4d4c297454928a78948e8a88b
Aliyyu Zen-Abdeen <babatundealiyyu411@gmail.com>
2025-12-19 19:48:06 +0200

    Delete week2 directory

--- week2/A2_T2.py
+++
@@ -1,7 +0,0 @@
-print("Program starting.")
-Carbrand =(input("Insert car brand:  "))
-CarModel = (input("Insert car Model:  "))
-sep =  input (f"{Carbrand}   sep"_" ")
-end = input (f"{CarModel}  end   "_"")
-
-print ("program ending.")
