1733c3475e199444e56f3eab3ad37a41c0a80139
Aliyyu Zen-Abdeen <babatundealiyyu411@gmail.com>
2025-12-23 20:16:36 +0200

    Update A9_T1.py

---
+++ Week 9/A9_T1.py
@@ -0,0 +1,28 @@
+########################################################
+# Task A9_T1
+# Developer First_name Last_name
+# Date 2025-12-23
+########################################################
+
+def main() -> None:
+    print("Program starting.\n")
+
+    total = 0.0
+
+    while True:
+        value_str = input("Insert a floating-point value (0 to stop): ")
+        if value_str == "0":
+            break
+
+        try:
+            value = float(value_str)  # may raise ValueError[web:252][web:254][web:257]
+            total += value
+        except ValueError:
+            print("Error! '{}' couldn't be converted to float.".format(value_str))
+
+    print("\nFinal sum is {:.2f}".format(total))  # two decimal places[web:255][web:258]
+    print("Program ending.")
+
+
+if __name__ == "__main__":
+    main()
